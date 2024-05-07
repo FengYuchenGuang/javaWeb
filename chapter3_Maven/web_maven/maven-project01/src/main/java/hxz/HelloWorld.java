7dcad662f3da306960c0dbcc720d25032e5b531c
FengYuchenGuang <ankskfp237@163.com>
2024-05-08 00:13:07 +0800

    2024/5/8

--- chapter3_Maven/web_maven/maven-project01/src/main/java/hxz/HelloWorld.java
+++ chapter3_Maven/web_maven/maven-project01/src/main/java/hxz/HelloWorld.java
@@ -6,5 +6,7 @@ package hxz;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello maven~~~");
+        System.out.println("hello maven~~~");
+        System.out.println("hello maven~~~");
     }
 }

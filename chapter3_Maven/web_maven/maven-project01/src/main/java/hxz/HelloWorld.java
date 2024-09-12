af47dcbd205f73ab0749bd6a048350be8c6d5321
FengYuchenGuang <ankskfp237@163.com>
2024-09-12 16:39:02 +0800

    2024/9/12

--- chapter3_Maven/web_maven/maven-project01/src/main/java/hxz/HelloWorld.java
+++ chapter3_Maven/web_maven/maven-project01/src/main/java/hxz/HelloWorld.java
@@ -1,5 +1,6 @@
 package hxz;
 
+
 import org.junit.jupiter.api.Test;
 
 /**
---
+++ chapter3_Maven/web_maven/maven-project03/src/main/java/hxz/HelloWorld.java
@@ -0,0 +1,16 @@
+package hxz;
+
+
+import org.junit.Test;
+
+/**
+ * @author hxz
+ */
+public class HelloWorld {
+    @Test
+    public static void main(String[] args) {
+        System.out.println("hello maven~~~");
+        System.out.println("hello maven~~~");
+        System.out.println("hello maven~~~");
+    }
+}
